1b7f84d08950de07cd4e98c505547cc8f4adaf0c
irfan-rumon <irfannisho8571@gmail.com>
2024-10-22 20:13:43 +0600

    refactor: account functionality

--- account/serializers.py
+++ account/serializers.py
@@ -3,13 +3,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-User = get_user_model()
+User = get_user_model()  # This uses your custom User model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'full_name', 'phone_number', 'street_address',
-                 'city', 'state', 'postal_code', 'country', 'date_of_birth')
+                  'city', 'state', 'postal_code', 'country', 'date_of_birth')
         read_only_fields = ('id',)
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -19,7 +19,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'password', 'password2', 'full_name', 'phone_number',
-                 'street_address', 'city', 'state', 'postal_code', 'country', 'date_of_birth')
+                  'street_address', 'city', 'state', 'postal_code', 'country', 'date_of_birth')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -27,13 +27,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        validated_data.pop('password2')  # Remove password2 since it's not needed in the creation process
+        user = User.objects.create_user(**validated_data)  # Use your custom create_user method
         return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs)
-        data['email'] = self.user.email
-        data['full_name'] = self.user.full_name
-        return data
+        data = super().validate(attrs)  # Get the default data (token, etc.)
+        data['email'] = self.user.email  # Add the user's email to the response
+        data['full_name'] = self.user.full_name  # Add the user's full name to the response
+        return data  # Return only the data dictionary, not a tuple
--- account/views.py
+++ account/views.py
@@ -1,14 +1,12 @@
-from rest_framework import viewsets, status, generics
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import get_user_model
 from .serializers import UserSerializer, UserRegistrationSerializer, CustomTokenObtainPairSerializer
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserRegistrationView(generics.CreateAPIView):
-    queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = UserRegistrationSerializer
 

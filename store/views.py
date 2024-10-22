b88d06b5a5ce782d19341c20afdcec1b497e95e0
irfan-rumon <irfannisho8571@gmail.com>
2024-10-22 18:44:32 +0600

    add CRUD API for the store entities

--- store/views.py
+++ store/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Category, Product, Cart, Order, OrderItem
+from .serializers import (
+    CategorySerializer, 
+    ProductSerializer, 
+    CartSerializer, 
+    OrderSerializer,
+    OrderItemSerializer
+)
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return []
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return []
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category = self.request.query_params.get('category', None)
+        if category is not None:
+            queryset = queryset.filter(category=category)
+        return queryset
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        # Check if product already in cart
+        product_id = request.data.get('product')
+        existing_item = Cart.objects.filter(
+            user=request.user,
+            product_id=product_id
+        ).first()
+
+        if existing_item:
+            # Update quantity instead of creating new item
+            quantity = int(request.data.get('quantity', 1))
+            existing_item.quantity += quantity
+            existing_item.save()
+            serializer = self.get_serializer(existing_item)
+            return Response(serializer.data)
+
+        return super().create(request, *args, **kwargs)
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        # Only allow staff to update order status
+        if not request.user.is_staff:
+            return Response(
+                {"detail": "Not authorized to update orders"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return super().update(request, *args, **kwargs)
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return OrderItem.objects.all()
+        return OrderItem.objects.filter(order__user=user)

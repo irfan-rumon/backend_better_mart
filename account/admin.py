ae5f9ef535b5a3eaaa9cbbbb2f69ed4322e6ba0e
irfan-rumon <irfannisho8571@gmail.com>
2024-10-23 14:53:37 +0600

    Setup: admin user

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    ordering = ('email',)
+    list_display = ('email', 'full_name', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'full_name')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('full_name', 'phone_number', 'date_of_birth', 'street_address', 'city', 'state', 'postal_code', 'country')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+# Register the custom UserAdmin
+admin.site.register(User, UserAdmin)

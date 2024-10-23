1c664b7e4347b38b930972f1b9f2432aa6b2d988
irfan-rumon <irfannisho8571@gmail.com>
2024-10-23 14:41:09 +0600

    Add jwt authentication configuration

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/store/', include('store.urls')),
-    path('api/accounts/', include('accounts.urls')),
+    path('api/account/', include('account.urls')),
 ]

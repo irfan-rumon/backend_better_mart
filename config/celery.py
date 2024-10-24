9fbd540661b2d255c9698116f11fc95d0960a810
irfan-rumon <irfannisho8571@gmail.com>
2024-10-24 12:52:16 +0600

    Add: celery.py

---
+++ config/celery.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Create Celery app
+app = Celery('configgit')
+
+# Load task modules from all registered Django app configs.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks in all installed apps
+app.autodiscover_tasks()
